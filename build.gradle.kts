d8d22f416b8d5ed9e0ce7d42946777ced2599bb5
Dandeline <blagovestdoukovy2@gmail.com>
2026-02-04 20:24:35 +0100

    Primer commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // JPA and Hibernate
+    implementation("org.hibernate:hibernate-core:6.2.7.Final")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+
+    // Postgre Driver
+    implementation("org.postgresql:postgresql:42.6.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
